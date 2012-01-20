81280e1169a7c444da765600eeb71c7db57854c5
Zachary Zimbler <zachzimbler@gmail.com>
2012-01-20 00:10:13 -0500

    update demo4

--- videos/demo4/data.js
+++ videos/demo4/data.js
@@ -1,40 +1,65 @@
-// main segments
-var S1 = new Segment("demo1/S1");
-var S2af = new Segment("demo1/S2af");
-var S2bf = new Segment("demo1/S2bf");
-var S3a1 = new Segment("demo1/S3a1");
-var S3a2 = new Segment("demo1/S3a2");
-var S3b1 = new Segment("demo1/S3b1");
-var S3b2 = new Segment("demo1/S3b2");
+//Black Dude
+	//Main Segments
+		var S1 = new Segment("demo4/S1");
+		var S1bd = new Segment("demo4/bd/1bd");	
+		var S3wd = new Segment("demo4/bd/3wd");	
+		var S5r1 = new Segment("demo4/bd/5r1");
+		var S7r2 = new Segment("demo4/bd/7r2");
+		var S9r3 = new Segment("demo4/bd/9r3");
+		var S11j = new Segment("demo4/bd/11j");
+	//After the Junction
+		//Correct Choice
+			var Sjc1t = new Segment("demo4/bd/j/c/1t");			
+			var Sjc2f = new Segment("demo4/bd/j/c/2f");		
+			var Sjc3t = new Segment("demo4/bd/j/c/3t");		
+		//Wrong Choice
+			var	Sjw1bd = new Segment("demo4/bd/j/w/1bd";
+			var Sjw2wdf = new Segment("demo4/bd/j/w/2wdf";
 
-// connection segments
-var S1r = new Segment("demo1/S1r");
-var S2as = new Segment("demo1/S2as");
-var S2bs = new Segment("demo1/S2bs");
+	//Main Segment Transistions	
+		var S2t = new Segment("demo4/bd/2t");
+		var S4t = new Segment("demo/bd/4t");
+		var S6t = new Segment("demo/bd/4t");
+		var S8t = new Segment("demo/bd/8t");
+		var S10t = new Segment("demo/bd/10t");
+//Post it Note Lady 
+	//Main Segments
+	//Transistions
+	
+//Black Lady
+	//Main Segments
+	//Transistions
+	
+//Video Order
 
-var S2ar = new Segment("demo1/S2ar");
-var S2br = new Segment("demo1/S2br");
+//First Choice to decide which customer
+S1.insertConnection(S1bd, "Black Dude", 3);
+S1.insertConnection(,"Postit Note Lady",4);
+S1.insertConnection(,"Black Lady",5);
 
-S1.insertConnection(S1r, "Wtf?", 1);
-S1.insertConnection(S2as, "Noob? I'm better than you!", 4);
-S1.insertConnection(S2bs, "It's for beating people up", 5);
+//Black Dude
+S1bd.insertDirect(2t);
+S2t.insertDirect(S3wd);
+S3wd.insertDirect(S4t);
+S4t.insertDirect(S5r1);
+S5r1.insertDirect(S6t);
+S6t.insertDirect(S7r2);
+S7r2.insertDirect(S8t);
+S8t.insertDirect(S9r3);
+S9r3.insertDirect(S10t);
+S10t.insertDirest(S11j);
 
-S1r.insertDirect(S1);
-S2as.insertDirect(S2af);
-S2bs.insertDirect(S2bf);
+//Going back?
+S11j.insertConnection(,"",);
+S11j.insertConnection(,"",);
+S11j.insertConnection(,"",);
 
-S2af.insertConnection(S2ar, "What you talking about?", 1);
-S2af.insertConnection(S3a1, "Team up", 4);
-S2af.insertConnection(S3a2, "Get Lost", 5);
+//Forward Choices
+S11j.insertConnection(Sjw1bd, "Choice 1", 3);
+S11j.insertConnection(Sjw1bd, "Choice 2", 4);
+S11j.insertConnection(Sjc1t, "Choice 3", 5);
 
-S2ar.insertDirect(S2af);
-
-S2bf.insertConnection(S2br, "The who with the what now?", 1);
-S2bf.insertConnection(S3b1, "Join Forces", 4);
-S2bf.insertConnection(S3b2, "Go Die", 5);
-
-S2br.insertDirect(S2bf);
-
-// timed event from 10 - 12
-S3b2.insertTimedEvent(S2br, 10, 12);
-
+//Wrong choice
+Sjw1bd.insertDirect(Sjw2wdf);
+Sjw2wdfinsertDirect(S1bd);
+//Correct Choices - nothing needed
