387521f1c245c41c605b238b1713603de54d1868
David <dzz0615@gmail.com>
2012-02-06 05:49:29 -0500

    stack.js added to compile script

--- view/js/compile.sh
+++ view/js/compile.sh
@@ -3,7 +3,7 @@
 cd src;
 
 java -jar ../../../bin/compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS \
-	--js=browser.js --js=connection.js \
+	--js=browser.js --js=connection.js --js=stack.js \
 	--js=function.js --js=model.js --js=player.js --js=segment.js \
 	--js=setvideourl.js --js=timedevent.js --js=raphael.js --js=theme.js \
 	--js_output_file=../go.js
