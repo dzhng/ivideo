70710d772e8fa79dd814531b4631e23b3d68bb8b
David <dzz0615@gmail.com>
2012-02-06 05:48:25 -0500

    added stack

---
+++ view/js/src/stack.js
@@ -0,0 +1,36 @@
+
+// store an array of data in a data structure, provide functions to add videos into the stack
+var Stack = function()
+{
+	this.stack = new Array();
+};
+
+// this is called when a new video is to be inserted into the stack
+Stack.prototype.insert = function(seg)
+{
+	this.stack.push(seg);
+};
+
+// remove the input segment from the stack
+Stack.prototype.remove = function(seg)
+{
+	for(var i = 0; i < this.stack.length; i++) {
+		// if segment found, remove and return
+		if(this.stack[i] == seg) {
+			this.stack.splice(i, 1);
+			return;
+		}
+	}
+};
+
+Stack.prototype.pull = function()
+{
+	if(this.stack.length > 1) {
+		return this.stack.shift();
+	} else if(this.stack.length == 1) {
+		return this.stack.pop();
+	} else {
+		return null;
+	}
+};
+
